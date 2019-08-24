7f20f719618a22e9ceb229ea5ee1c1ab5a45c4e8
abdulrauf11 <rauf11.abdul@gmail.com>
2019-08-24 18:42:49 +0500

    added work filters

--- src/templates/portfolio.js
+++ src/templates/portfolio.js
@@ -11,6 +11,7 @@ const Portfolio = ({ data }) => {
   const portfolio = data.markdownRemark.frontmatter
   const settings = {
     dots: true,
+    arrows: false,
     infinite: true,
     speed: 500,
     centerMode: true,
